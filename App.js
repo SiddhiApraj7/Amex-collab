4826b2162cc5d274defd9fd3643168758be0e84d
Ashish Kumar <heyitsashishkumar@gmail.com>
2023-06-03 15:42:40 +0530

    added amplify

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+Amplify.configure(awsExports);
+
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working afafai on your app!</Text>
       <StatusBar style="auto" />
+      <SignOutButton />
     </View>
   );
 }
+
+export default withAuthenticator(App);
+
+// retrieves only the current value of 'user' from 'useAuthenticator'
+const userSelector = (context) => [context.user]
+
+const SignOutButton = () => {
+  const { user, signOut } = useAuthenticator(userSelector);
+  return (
+    <Pressable onPress={signOut} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>Hello, {user.username}! Click here to sign out!</Text>
+    </Pressable>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
