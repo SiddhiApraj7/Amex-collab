12da63ff8bd05268d11f9828b0d89d11a0665bb0
tanisha <tanisha22daharwal@gmail.com>
2023-06-09 13:21:47 +0530

    create-voucher successful

--- amplify/backend/function/amBackendGateway/src/app.js
+++ amplify/backend/function/amBackendGateway/src/app.js
@@ -232,10 +232,10 @@ app.post('/create-serviceProvider', async function(req, res) {
 
 
 app.post('/create-voucher', async function(req, res) {
-  const { voucherAmount, PhoneNumberSP, PhoneNumberB, PhoneNumberPvtOrg } = req.body;
-
+  const { voucherAmount, PhoneNumberSP, PhoneNumberB, PhoneNumberPvtOrg, voucherRedeemed, voucherSPId, voucherBeneficiaryId, PvtOrgById} = req.body;
+  
   try {
-    const serviceProvider = await prisma.serviceProvider.findUnique({
+    const serviceProvider = await prisma.serviceProvider.findFirst({
       where: {
         Users : {
           phoneNumber: PhoneNumberSP
@@ -247,7 +247,7 @@ app.post('/create-voucher', async function(req, res) {
       return res.status(404).json({ error: 'Service provider not found' });
     }
 
-    const beneficiary = await prisma.beneficiary.findUnique({
+    const beneficiary = await prisma.beneficiary.findFirst({
       where: {
         Users : {
           phoneNumber: PhoneNumberB
@@ -259,7 +259,7 @@ app.post('/create-voucher', async function(req, res) {
       return res.status(404).json({ error: 'Beneficiary not found' });
     }
 
-    const pvtOrg = await prisma.pvtOrg.findUnique({
+    const pvtOrg = await prisma.pvtOrg.findFirst({
       where: {
         Users : {
           phoneNumber: PhoneNumberPvtOrg
