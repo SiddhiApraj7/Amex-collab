a1b74990965f5076be0cc086e893a4828565c0c8
Siddhi Apraj <2021mcb1247@iitrpr.ac.in>
2023-06-12 21:36:21 +0530

    fix footer

--- src/screens/BeneficiaryHomePage.js
+++ src/screens/BeneficiaryHomePage.js
@@ -1,12 +1,12 @@
-import { View, Text, SafeAreaView, TextInput, Image, Button,ScrollView , StyleSheet,TouchableOpacity} from 'react-native';
+import { View, Text, SafeAreaView, TextInput, Image, Button, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
-import {Ionicons} from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import Walletcard from '../components/Walletcard';
 import { BackHandler } from 'react-native';
 import axios from 'axios';
 import { AppContext } from "../../AppContext";
-import { useContext , useState} from "react";
+import { useContext, useState } from "react";
 
 
 const BeneficiaryHomePage = () => {
@@ -16,7 +16,7 @@ const BeneficiaryHomePage = () => {
   const [bankName, setBankName] = useState('');
 
   //const { phoneNumber, setPhoneNumber } = useContext(AppContext);
-  
+
   useFocusEffect(
     React.useCallback(() => {
       const onBackPress = () => {
@@ -51,136 +51,127 @@ const BeneficiaryHomePage = () => {
         navigation.navigate('login'); // Replace 'Login' with the name of your login screen
       }, 3000); */ // Redirect to login screen after 3 seconds
     }
-  } 
+  }
 
   fetchBenificiaryInfo();
 
-    const textrupi = (
-    
-        <Text className="text-xs"> E-RUPI</Text>
-    )
-    const textrupee = (
-    
-        <Text className="text-xs" >E-RUPEE</Text>
-    )
+  const textrupi = (
+
+    <Text className="text-xs"> E-RUPI</Text>
+  )
+  const textrupee = (
+
+    <Text className="text-xs" >E-RUPEE</Text>
+  )
   return (
     <SafeAreaView className="bg-white h-full">
-        <View className="items-center bg-white">
+      <View className="items-center bg-white">
+        <Image
+          className="h-36 w-52 mt-4"
+          source={require('../../assets/e-rupi.png')}></Image>
 
-        
-            <Image
-            className="h-36 w-52 mt-4"
-            
-            source = {require('../../assets/e-rupi.png')}></Image>
-    
         <View >
-        <View className="flex-row gap-2 ml-5 w-96 justify-between">
-              <View className="flex-row gap-2">
-                <Ionicons name="person-circle" size={36}></Ionicons>
-                <View className="pb-2">
+          <View className="flex-row gap-2 ml-5 w-96 justify-between">
+            <View className="flex-row gap-2">
+              <Ionicons name="person-circle" size={36}></Ionicons>
+              <View className="pb-2">
                 <Text className="font-medium text-lg mr-7">{firstName} {lastName}</Text>
                 {/* <Text className="font-light text-sm mr-7">Infosys - HR Head</Text> */}
-                </View>
-                </View>
-                <View className="pt-1 mr-5">
-                <Text className="font-medium text-lg">(bankName)</Text>
-                </View>
+              </View>
             </View>
+            <View className="pt-1 mr-5">
+              <Text className="font-medium text-lg">(bankName)</Text>
+            </View>
+          </View>
 
-            <View><Text className="font-light text-center mt-5">TOTAL BALANCE</Text></View>
-            <View><Text className="font-bold text-xl text-center mt-3 mb-3">1000 e$</Text></View>
+          <View><Text className="font-light text-center mt-5">TOTAL BALANCE</Text></View>
+          <View><Text className="font-bold text-xl text-center mt-3 mb-3">1000 e$</Text></View>
 
-            <View className="flex-row gap-6 ml-1">
+          <View className="flex-row gap-6 ml-1">
             <TouchableOpacity onPress={() => {
               navigation.navigate("e_rupi_wallet");
             }}>
-            <Walletcard children={textrupi}/>
+              <Walletcard children={textrupi} />
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={()=> {
+            <TouchableOpacity onPress={() => {
               navigation.navigate("e_rupee_wallet");
             }}>
-            <Walletcard children={textrupee} />
+              <Walletcard children={textrupee} />
             </TouchableOpacity>
 
             <View>
-            <View className="">
-            <View className="w-38 h-46 mx-0 mr-6 py-5 pl-5 pr-5 text-center rounded-2xl mt-5 bg-blue-200" >
-              <View className="my-auto ml-4">
-              <Ionicons name="documents-outline" size={56} ></Ionicons>
+              <View className="">
+                <View className="w-38 h-46 mx-0 mr-6 py-5 pl-5 pr-5 text-center rounded-2xl mt-5 bg-blue-200" >
+                  <View className="my-auto ml-4">
+                    <Ionicons name="documents-outline" size={56} ></Ionicons>
+                  </View>
+                  <Text className="text-xs">Digital Safe</Text>
+                </View>
               </View>
-              <Text className="text-xs">Digital Safe</Text>
             </View>
-    </View>
-            </View>
-            
-        </View>
 
-        <View className="flex-row">
+          </View>
+
+          <View className="flex-row">
             <Image source={require("../../assets/amex.jpeg")} className="w-20 h-20 p-5 ml-6 mt-5"></Image>
             <View className="my-auto ml-4">
               <Ionicons name="add-circle-outline" size={26} ></Ionicons>
-              </View>
-        </View>
+            </View>
+          </View>
 
-        <View>
+          <View>
             <Text className="font-light text-center mt-5">GOVERNMENT SCHEMES</Text>
-        </View>
+          </View>
 
-        <ScrollView horizontal={true} className="mt-5">
+          <ScrollView horizontal={true} className="mt-5">
             <View className="flex-row text-center">
-            <View className="bg-gray-200 ml-6 mr-2 p-2 h-24 align-middle text-center rounded-lg align-center">
-              <Image source={require("../../assets/Vaccine.png")} className="w-20 h-20 "></Image>
-              {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
-              
+              <View className="bg-gray-200 ml-6 mr-2 p-2 h-24 align-middle text-center rounded-lg align-center">
+                <Image source={require("../../assets/Vaccine.png")} className="w-20 h-20 "></Image>
+                {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
+
+              </View>
+
+              <View className="bg-gray-200 ml-1 mr-2 p-4 h-24 align-middle text-center rounded-lg align-center">
+                <Image source={require("../../assets/Emergency-Management.png")} className="w-16 h-16 rounded-ful "></Image>
+                {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
+
+              </View>
+
+              <View className="bg-gray-200 ml-1 mr-2 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
+                <Image source={require("../../assets/scholar.png")} className="w-14 h-14 rounded-ful "></Image>
+                {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
+
+              </View>
+
+              <View className="bg-gray-200 ml-1 mr-2 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
+                <Image source={require("../../assets/pension.png")} className="w-14 h-14 rounded-ful "></Image>
+                {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
+
+              </View>
+              <View className="bg-gray-200 ml-1 mr-6 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
+                <Image source={require("../../assets/food.jpeg")} className="w-14 h-14 rounded-ful "></Image>
+                {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
+
+              </View>
             </View>
 
-            <View className="bg-gray-200 ml-1 mr-2 p-4 h-24 align-middle text-center rounded-lg align-center">
-              <Image source={require("../../assets/Emergency-Management.png")} className="w-16 h-16 rounded-ful "></Image>
-              {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
-              
-            </View>
+          </ScrollView>
 
-            <View className="bg-gray-200 ml-1 mr-2 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
-              <Image source={require("../../assets/scholar.png")} className="w-14 h-14 rounded-ful "></Image>
-              {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
-              
-            </View>
-
-            <View className="bg-gray-200 ml-1 mr-2 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
-              <Image source={require("../../assets/pension.png")} className="w-14 h-14 rounded-ful "></Image>
-              {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
-              
-            </View>
-            <View className="bg-gray-200 ml-1 mr-6 w-30 p-5 h-24 align-middle text-center rounded-lg align-center">
-              <Image source={require("../../assets/food.jpeg")} className="w-14 h-14 rounded-ful "></Image>
-              {/*<Ionicons name="medkit-outline" className="mx-auto"size={56}></Ionicons>*/}
-              
-            </View>
-
-            </View>
-
-        </ScrollView>
-            
-               
-           
         </View>
-
-        
-    </View>
-
-    <View className="bg-gray-300 h-36 mt-20 rounded-lg">
-        <View className="flex-row gap-10 mx-auto text-center p-1 ml-0" >
+      </View>
+      <View className="bg-gray-300 rounded-lg pt-1" style={{position: 'absolute', left:0, right:0, bottom:25, flex:1}}>
+          <View className="flex-row gap-10 justify-evenly" >
           <View className="text-center items-center"><Ionicons name="home-outline" size={20}></Ionicons><Text className="text-xs">Dashboard</Text></View>
           <View className="text-center items-center"><Ionicons name="build-outline" size={20}></Ionicons><Text className="text-xs">Select Role</Text></View>
           <View className="text-center items-center"><Ionicons name="wallet-outline" size={20}></Ionicons><Text className="text-xs">Wallets</Text></View>
           <View className="text-center items-center"><Ionicons name="person-outline" size={20}></Ionicons><Text className="text-xs">Profile</Text></View>
-        </View>
-        
+      </View>
+
       </View>
     </SafeAreaView>
   )
-          }
+}
 
 
 
